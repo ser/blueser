f4e148c294645d21bba8036c395ea3281d5f73f5
ᎠᎡ. Ѕϵrgϵ Ѵictor <ser@users.noreply.github.com>
2025-12-03 16:49:36 +0700

    + remove ms edge

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,13 +3,13 @@
 set -ouex pipefail
 
 log() {
-  echo "=== $* ==="
+	echo "=== $* ==="
 }
 
 ### Install packages
 
 # Add Google repo for antigravity
-tee /etc/yum.repos.d/antigravity.repo << EOL
+tee /etc/yum.repos.d/antigravity.repo <<EOL
 [antigravity-rpm]
 name=Antigravity RPM Repository
 baseurl=https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm
@@ -18,7 +18,7 @@ gpgcheck=0
 EOL
 
 # Add MS repo for edge
-tee /etc/yum.repos.d/edge.repo << EOL
+tee /etc/yum.repos.d/edge.repo <<EOL
 [edge-rpm]
 name=Edge RPM Repository
 baseurl=https://packages.microsoft.com/yumrepos/edge/
@@ -26,57 +26,57 @@ enabled=1
 gpgcheck=0
 EOL
 
-
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 
 PACKAGES=(
-  antigravity
-  chromium
-  genisoimage
-  git-crypt
-  gnome-tweaks
-  htop
-  incus
-  libpq-devel
-  kitty
-  microsoft-edge-stable
-  netcat
-  pacrunner
-  perl-hivex
-  pssh
-  shorewall
-  snapd
-  solaar
-  virt-install
-  virt-manager
-  wimlib-utils
+	antigravity
+	chromium
+	genisoimage
+	git-crypt
+	gnome-tweaks
+	htop
+	incus
+	libpq-devel
+	kitty
+	#microsoft-edge-stable
+	netcat
+	pacrunner
+	perl-hivex
+	pssh
+	shorewall
+	snapd
+	solaar
+	virt-install
+	virt-manager
+	wimlib-utils
 )
 
 REMOVALS=(
-  firewalld
-  starship
+	firewalld
+	starship
 )
 
 # this installs a package from fedora repos
 log "Installing packages using dnf5..."
 dnf5 install --setopt=install_weak_deps=False -y \
-  ${PACKAGES[@]}
+	"${PACKAGES[@]}"
 
 # this removes packages we do not like
 log "Removing packages using dnf5..."
 dnf5 remove -y \
-  ${REMOVALS[@]}
+	"${REMOVALS[@]}"
 
 # Use a COPR
 dnf5 -y copr enable taw/joplin
 dnf5 -y copr enable petersen/nix
-dnf5 -y copr enable 0x444d/looking-glass 
+dnf5 -y copr enable 0x444d/looking-glass
 
 # Install from COPRs
-dnf5 install -y joplin looking-glass-client nix
+dnf5 install --setopt=install_weak_deps=False -y \
+	joplin looking-glass-client nix
 
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable taw/joplin
 dnf5 -y copr disable petersen/nix
-dnf5 -y copr disable 0x444d/looking-glass 
+dnf5 -y copr disable 0x444d/looking-glass
